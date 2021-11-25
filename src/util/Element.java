ddbc8ba247c69e24d03e5d47f4612ca0acdd2692
Alen Bijelic <alen.bijelic@heig-vd.ch>
2021-11-25 14:46:03 +0100

    Point 4 OK

--- src/util/Element.java
+++ src/util/Element.java
@@ -1,12 +1,8 @@
-package ch.heigvd.poo.labo7.jeanrenaud_bijelic;
+package util;
 
 public class Element<T> {
     protected T data;
     protected Element<T> next;
-
-    public T getData() {
-        return data;
-    }
 
     public Element(T data, Element<T> next) {
         this.data = data;
