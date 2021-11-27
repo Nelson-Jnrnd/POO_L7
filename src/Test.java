a29e3d398af21f4f3e055a4dcf9d07fccbffb4aa
Alen Bijelic <alen.bijelic@heig-vd.ch>
2021-11-27 14:40:46 +0100

    test full ok

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,17 @@
 import hanoi.Hanoi;
 import hanoi.HanoiDisplayer;
-import hanoi.gui.JHanoi;
 
 public class Test {
     public static void main(String[] args) {
-        if(args.length != 1){
-            System.out.println("No enough args");
-            return;
+        switch (args.length) {
+            case 0: testGui();
+                    break;
+            case 1:
+                int disks = Integer.parseInt(args[0]);
+                testConsole(disks);
+                break;
+            default: System.out.println("Too much args");
         }
-        testGui();
-        int disks = Integer.parseInt(args[0]);
-
-        testConsole(disks);
     }
 
     public static void testConsole(int disks){
@@ -21,7 +21,8 @@ public class Test {
         displayer.display(hanoi);
         hanoi.solve();
     }
+
     public static void testGui(){
-        JHanoi monJhanoi = new JHanoi();
+        new hanoi.gui.JHanoi();
     }
 }
